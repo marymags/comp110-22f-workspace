97a42560ab69a9d2a469405b3646ed38b711c106
Maggie Saunders <marymags@ad.unc.edu>
2022-10-25 18:49:11 -0400

    thisisamessage

--- exercises/ex08/data_utils.py
+++ exercises/ex08/data_utils.py
@@ -1,5 +1,8 @@
 """Dictionary related utility functions."""
 
-__author__ = ""
+__author__ = "730576725"
 
 # Define your functions below
+
+def read_csv_rows(thing: str) -> list[dict[str, str]]: 
+    for item in 
