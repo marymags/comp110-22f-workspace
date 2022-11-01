8842f37036798bf9d1384bf20f55bfa4df6658fa
Maggie Saunders <marymags@ad.unc.edu>
2022-11-01 14:56:33 -0400

    message

---
+++ lessons/cl07.py
@@ -0,0 +1,48 @@
+"""Examples of a class and objects."""
+
+class Profile:
+    handle: str
+    followers: int
+    is_private: bool
+
+    def __init__(self, handle: str):
+        """Constructor initializes attributes!"""
+        self.handle = handle
+        self.followers = 0
+        self.is_private = False
+
+    def tweet(self, msg: str) -> None:
+        """An example of a method."""
+        print(f"@{self.handle} tweets {msg}")
+
+
+my_profile: Profile = Profile("maggiesaunders")
+my_profile.tweet("Hello, world.")
+
+
+
+class Dog:
+    name: str
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def speak(self) -> str:
+        return f"{self.name}: woof"
+
+
+class Cat:
+    name:str
+
+    def __init__(self, name: str):
+        self.name = name
+
+    def speak(self) -> str:
+        return f"{self.name}: meow"
+
+
+fido: Cat = Cat("Cleo")
+leo: Dog = Dog("Loki")
+
+print(fido.speak())
+print(leo.speak())
